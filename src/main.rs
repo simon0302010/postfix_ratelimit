f6ccc29269570255ffaa1e23c801a6c73ac4b0f3
simon0302010 <simon0302010@gmail.com>
2025-12-26 12:04:41 +0100

    Fixed stupid mistake

--- src/main.rs
+++ src/main.rs
@@ -155,7 +155,7 @@ async fn load_db(disk_path: PathBuf) -> Result<Connection, Box<dyn Error>> {
         .call(move |conn_mem| {
             let conn_disk = rusqlite::Connection::open(&disk_path)?;
             let backup = rusqlite::backup::Backup::new(&conn_disk, conn_mem)?;
-            backup.run_to_completion(-1, Duration::from_millis(0), None)
+            backup.run_to_completion(1_000, Duration::from_millis(0), None)
         })
         .await
         .unwrap_or_else(|e| {
@@ -172,7 +172,7 @@ async fn save_db(db_mem: &Connection, disk_path: PathBuf) -> Result<(), Box<dyn 
         .call(move |conn_mem| {
             let mut conn_disk = rusqlite::Connection::open(&disk_path)?;
             let backup = rusqlite::backup::Backup::new(conn_mem, &mut conn_disk)?;
-            backup.run_to_completion(-1, Duration::from_millis(0), None)
+            backup.run_to_completion(1_000, Duration::from_millis(0), None)
         })
         .await
         .unwrap_or_else(|e| {
